77722e71605a13e68292457352a6ebafe6c8515e
đức cao <endgame1462005@gmail.com>
2026-09-11 15:35:56 +0700

    done lesson 19

---
+++ file-service/src/main/java/com/devteria/file/service/FileService.java
@@ -0,0 +1,27 @@
+package com.devteria.file.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class FileService {
+
+    public Object upload(MultipartFile file) throws IOException {
+        Path folder = Path.of("C:/upload");
+        String fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        String fileName = Objects.isNull(fileExtension)?
+                UUID.randomUUID().toString()
+                : UUID.randomUUID().toString() + "." + fileExtension;
+        Path filePath = folder.resolve(fileName).toAbsolutePath().normalize().toAbsolutePath();
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        return null;
+    }
+}
